da35d146b5b93fc4cca7b4cdb69ba8cedef504cc
Knrc <kontakt.mikey@gmail.com>
2022-12-04 22:01:34 +0100

    v.0.1.7 | Menu Buttons, FoodBowl

---
+++ Assets/Scripts/ClickableObjects.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjects : MonoBehaviour
+{
+    public Renderer Player;
+    public GameObject StatusMenu;
+    public GameObject Menu;
+    void Start()
+    {
+        Player.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Player.GetComponent<SpriteRenderer>().enabled = false;
+        Menu.SetActive(true);
+        StatusMenu.SetActive(false);
+        
+    }
+
+    private void OnMouseOver()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color (1f,1f,1f, 0.8f);
+    }
+
+        private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color (1f,1f,1f, 1f);
+    }
+}
